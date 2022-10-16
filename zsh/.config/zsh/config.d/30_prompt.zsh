73066547cc8e03c4114ab539e4f3ad0d3da19dc6
Erin <github@eritbh.me>
2022-10-16 18:44:55 -0400

    zsh: that is not how vcs_info works rip

--- zsh/.config/zsh/config.d/30_prompt.zsh
+++ zsh/.config/zsh/config.d/30_prompt.zsh
@@ -6,7 +6,8 @@ zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{
 zstyle ':vcs_info:*' formats '(%b) '
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 autoload -Uz vcs_info
-precmd_functions+=vcs-info
+precmd_functions+="vcs_info"
 
 # Prompt shows hostname if connected to a remote or if root
 PROMPT="%B$([ -n "$is_remote" ] || [ "$EUID" = 0 ] && echo "%F{magenta}%m%f ")%F{$([ "$EUID" = 0 ] && echo "red" || echo "cyan")}%n%f %F{blue}%~%f%b \${vcs_info_msg_0_}%B%#%b "
+

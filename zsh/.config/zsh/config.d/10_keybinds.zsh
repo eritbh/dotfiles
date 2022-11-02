01d99a92dc3f32307f9dadece22a9696d356c336
Erin <github@eritbh.me>
2022-11-02 11:22:15 -0400

    zsh: add keybind ^Z for quick `fg`

---
+++ zsh/.config/zsh/config.d/10_keybinds.zsh
@@ -0,0 +1,12 @@
+# Bind ^Z on an empty line to resume the last suspended job
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
